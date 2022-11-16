8f7f6f05398bc260dd4f222efcfb0140b35c9d97
Subscaaper <63253193+Subscaaper@users.noreply.github.com>
2022-11-16 22:53:23 +0100

    Added Class Code Convention
    Updated File Code Conventions.docx, added Variables

---
+++ Project Documentation/Code Conventions.cs
@@ -0,0 +1,34 @@
+﻿// Using
+// Other Libraries
+using Unity.Engine;
+// Own Class
+// using Other.OwnClass; 
+
+// Adjust namespaces, according to Folder structure
+namespace DefaultNamespace
+{
+    public class Code_Convention
+    {
+        // Class Variables
+        // private Variable beginns with a underscore
+        // [SerializeField] only when needed 
+        [SerializeField] private string _nameOfCar;
+        
+        // Method Variables
+        public void MethodVariables()
+        {
+            // Variable without underscore
+            int number = 0;
+            // use of var, initialize it, so var knows what type will be stored
+            var text = "";
+            
+            // Other situations to use var
+            // foreach
+            var userList = new List<string>();
+            foreach (var item in userList)
+            {
+                // Do smth
+            }
+        }
+    }
+}
