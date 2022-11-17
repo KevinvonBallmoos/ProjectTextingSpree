6f227abc519046925b58fb3cb6909d05d7370b16
Subscaaper <63253193+Subscaaper@users.noreply.github.com>
2022-11-17 23:18:17 +0100

    Updated C# Part of Code Conventions TODO: Comments, Sorting of Variables everywhere

--- Project Documentation/Code Conventions.cs
+++ Project Documentation/Code Conventions.cs
@@ -1,21 +1,35 @@
-﻿// Using
+﻿// ## 2.1 Using ## //
 // Other Libraries
 using Unity.Engine;
 // Own Class
 // using Other.OwnClass; 
 
 // Adjust namespaces, according to Folder structure
+// Curly Brackets are below the Code and not on the same Line
 namespace DefaultNamespace
 {
     public class Code_Convention
     {
-        // Class Variables
+        // ## 2.2 Class Variables ## //
         // private Variable beginns with a underscore
         // [SerializeField] only when needed 
         [SerializeField] private string _nameOfCar;
         
-        // Method Variables
-        public void MethodVariables()
+        // Readonly: Can be initialized at Compiletime or Runtime
+        private readonly string PathToStoryFile;
+        // Const: Can only be initialized at Compiletime
+        private const int MaxSpeed = 120; 
+        
+        
+        // ## 2.3 Methods ## //
+        // Method Name starts with Verb and UpperCamelCase
+        private string GetCarName()
+        {
+            return _nameOfCar;
+        }
+        
+        // ## 2.4 Method Variables ## //
+        public void ReadMiscellaneous()
         {
             // Variable without underscore
             int number = 0;
@@ -29,6 +43,21 @@ namespace DefaultNamespace
             {
                 // Do smth
             }
+            
+            // ## 2.5 Miscellaneous ## //
+            // Simplified If
+            if (number == 1)
+                number = 12;
+            
+            // Simplified For
+            for (int i = 0; i < 5; i++)
+                number += i;
+            // Simplified Foreach
+            foreach (var item in userList)
+                text += user + ";";
+            // Simplifiedd While
+            while (int i < number)
+                text = GetCarName();
         }
     }
 }
