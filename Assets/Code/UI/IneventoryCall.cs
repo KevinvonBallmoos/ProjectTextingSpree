a9f823dac866c27fc8c02589337375234f0a69bc
Kastriot Dulla <101819640+DulKas@users.noreply.github.com>
2022-12-25 13:47:42 +0100

    Trying out some things with the commits.

--- Assets/Code/UI/IneventoryCall.cs
+++ Assets/Code/UI/IneventoryCall.cs
@@ -19,6 +19,7 @@ public class IneventoryCall : MonoBehaviour
 
     /// <summary>
     /// Calls the inventory and disables the map.
+    /// This comment is only for commit purpuses.
     /// </summary>
     private void CallInventory()
     {
